1ec8e498ad8d5cc0fc16b817b45e4a21a82ae26c
Ubuntu <ubuntu@ip-172-26-1-69.ap-northeast-2.compute.internal>
2026-06-06 13:01:59 +0000

    Modify allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ KAKAO_SECRET_KEY = secrets['KAKAO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['aivleteam19literacy.run.goorm.io']
+ALLOWED_HOSTS = ['api.liquest.click']
 
 # Application definition
 
